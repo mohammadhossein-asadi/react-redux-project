459b5610cb89c7e8386334fa3b23c5653fa0b5a5
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-06-12 18:49:56 +0430

    commit reducers

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,21 @@
+import { ActionTypes } from "../contants/action-types";
+
+const initialState = {
+  products: [
+    {
+      id: 1,
+      title: "Mohammadhossein",
+      category: "programmer",
+    },
+  ],
+};
+
+export const productReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SET_PRODUCTS:
+      return state;
+
+    default:
+      return state;
+  }
+};
